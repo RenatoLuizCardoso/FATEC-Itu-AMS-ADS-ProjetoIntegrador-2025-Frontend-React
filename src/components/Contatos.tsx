c4ef072c9903e7d0a1e498da3a9b0e02473d2b8e
AngeloModanez <angelomodanez15@gmail.com>
2025-05-08 16:49:14 -0300

    :sparkles: feat: Improvement on Component Contatos

--- src/components/Contatos.tsx
+++ src/components/Contatos.tsx
@@ -1,7 +1,7 @@
 import { Clock, Mail, MapPin, MessageCircle, Phone } from 'lucide-react';
 
 export function Contatos() {
-  const style = 'underline hover:text-amber-600 transition-colors';
+  const style = 'underline hover:text-amber-700 transition-colors';
 
   const contatos = [
     {
@@ -18,13 +18,14 @@ export function Contatos() {
       label: 'Endereço',
       content: (
         <a
-          href="https://www.google.com/maps/place/V%C3%B4+Guerino+Bar/@-23.0824954,-47.2922158,13z/data=!4m10!1m2!2m1!1srestaurante+indaiatuba!3m6!1s0x94c8b3573f959ad5:0x39bc0975d6ca6617!8m2!3d-23.0825082!4d-47.2200975!15sChZyZXN0YXVyYW50ZSBpbmRhaWF0dWJhWhgiFnJlc3RhdXJhbnRlIGluZGFpYXR1YmGSARFmYW1pbHlfcmVzdGF1cmFudKoBUBABKg8iC3Jlc3RhdXJhbnRlKAwyHxABIhu7bdN5mfNBe5rpOALsMLzKTaQxqiKezCabTosyGhACIhZyZXN0YXVyYW50ZSBpbmRhaWF0dWJh4AEA!16s%2Fg%2F1q5gmbfkl?entry=ttu&g_ep=EgoyMDI1MDUwNi4wIKXMDSoASAFQAw%3D%3D"
+          href="https://www.google.com/maps/place/V%C3%B4+Guerino+Bar/@-23.0824954,-47.2922158,13z/data=!4m10!1m2!2m1!1srestaurante+indaiatuba!3m6!1s0x94c8b3573f959ad5:0x39bc0975d6ca6617!8m2!3d-23.0825082!4d-47.2200975"
           target="_blank"
           rel="noopener noreferrer"
           className={style}
         >
-          Rua Padre Manoel da Nóbrega, 271 - Vila Almeida, Indaiatuba - SP,
-          13330-650
+          Rua Padre Manoel da Nóbrega, 271 <br />
+          Vila Almeida, Indaiatuba - SP <br />
+          CEP 13330-650
         </a>
       ),
     },
@@ -47,43 +48,41 @@ export function Contatos() {
       label: 'Telefone',
       content: '+55 14 54773-1275',
     },
-    {
-      icon: Clock,
-      label: 'Horários',
-      content: (
-        <>
-          Ter a Qui: 18h30 – 22h30 <br />
-          Sex e Sáb: 18h30 – 23h30 <br />
-          Dom: 12h – 16h
-        </>
-      ),
-    },
   ];
 
   return (
-    <section className="w-screen py-16 text-amber-900 bg-yellow-50">
-      <div className='max-w-7xl mx-auto px-6 space-y-24"'>
-        <header className="max-w-4xl mx-auto mb-12 text-center">
-          <h2 className="text-3xl md:text-4xl font-bold mb-4">Contato</h2>
-          <p className="text-base md:text-lg">
-            Entre em contato conosco ou confira nossos horários de
-            funcionamento.
+    <section className="w-screen py-8 text-amber-900 bg-yellow-50">
+      <div className="mx-auto px-12 max-w-6xl space-y-24 md:px-12">
+        <header className="mx-auto mb-8">
+          <h2 className="text-4xl md:text-5xl font-bold">Contatos</h2>
+          <hr className="border-2 rounded-xl w-25 mb-4 border-lime-800" />
+          <p className="text-lg md:text-xl">
+            Deseja fazer uma reserva ou tem alguma dúvida? Entre em contato
+            conosco. Nossa equipe terá o prazer em atendê-lo.
           </p>
         </header>
 
-        <div className="max-w-6xl mx-auto grid grid-cols-1 gap-8">
+        <div className="mx-auto grid grid-cols-1 gap-8">
           {contatos.map(({ icon: Icon, label, content }) => (
-            <div
-              key={label}
-              className="flex items-start gap-4 bg-white rounded-xl p-4 md:p-6 transition hover:shadow-md"
-            >
-              <Icon className="w-8 h-8 text-amber-700 flex-shrink-0" />
+            <div key={label} className="flex gap-4">
+              <Icon size={20} />
               <div className="space-y-1">
-                <h3 className="text-lg font-bold">{label}:</h3>
-                <div className="text-base">{content}</div>
+                <h3 className="text-lg md:text-xl font-bold">{label}</h3>
+                <div className="text-base md:text-lg text-amber">{content}</div>
               </div>
             </div>
           ))}
+          <div className="space-y-1">
+            <h3 className="text-lg md:text-xl font-bold">
+              Horário de Funcionamento
+            </h3>
+            <div className="text-base md:text-lg text-amber">
+              Terça a Quinta: 18h às 23h <br />
+              Sexta e Sábado: 12h às 15h e 18h às 00h <br />
+              Domingo: 12h às 17h <br />
+              Segunda: Fechado
+            </div>
+          </div>
         </div>
       </div>
     </section>
