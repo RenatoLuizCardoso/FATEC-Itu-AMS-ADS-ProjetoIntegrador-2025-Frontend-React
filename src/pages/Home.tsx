c45e7007f3768329856c89352f02025d7e27e0d5
Murilo Santos <151096774+muriisantuss@users.noreply.github.com>
2025-05-09 17:23:40 -0300

    📝 docs: merged from pull request (#16)
    * :sparkles: feat: structure carousel
    
    * :sparkles: feat: improve carousel UX with smooth scroll, updated navigation and styling
    
    * :lipstick: style: update Chevron icon size to 28 using size prop
    
    * :memo: docs: add description template for PR
    
    ---------
    
    Co-authored-by: MURILO DOS SANTOS <1781432412016@fatecitu.local>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,19 @@
+// import { HelloWorld } from '@components/hello-World';
 import { Contatos } from '@components/Contatos';
 import { Hero } from '@components/Hero';
-// import { HelloWorld } from '@components/helloWorld';
 import { Navbar } from '@components/Navbar';
+import { Carrousel } from '@components/carrousel';
 import { QuemSomos } from '@components/quemSomos';
 
 export function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-
       <main>
         {/* Aqui vai o sessão hero */}
         <Hero />
         <QuemSomos />
+        <Carrousel />
         <Contatos />
       </main>
       {/* Coloque o footer aqui */}
