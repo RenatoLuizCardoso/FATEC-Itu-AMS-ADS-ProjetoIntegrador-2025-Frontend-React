6c84d8b49f30ef524313e2ac4e8a700e24cd8ddb
Murilo Santos <151096774+muriisantuss@users.noreply.github.com>
2025-05-18 21:40:52 -0300

    :recycle: refactor: improve layout structure and background handling

--- src/components/sections/hero/heroLayout.tsx
+++ src/components/sections/hero/heroLayout.tsx
@@ -1,18 +1,22 @@
-export function Hero() {
-  const title = 'Nome & Restaurante';
-  const button = [{ info: 'Reserva' }, { info: 'Cardápio' }];
+type HeroButton = {
+  info: string;
+};
 
+type HeroProps = {
+  title: string;
+  buttons: HeroButton[];
+  img: string;
+};
+
+export function HeroLayout({ title, buttons, img }: HeroProps) {
   return (
     <section className="relative flex min-h-[80vh] w-screen items-center bg-center bg-cover py-16">
-      {/* Sobrepocição na imagem */}
       <div className="absolute inset-0 z-10 bg-black/40">{''}</div>
 
-      {/* Imagem de fundo */}
       <div
         className="absolute inset-0 bg-center bg-cover bg-no-repeat"
         style={{
-          backgroundImage:
-            "url('https://images.unsplash.com/photo-1682778418768-16081e4470a1?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+          backgroundImage: `url(${img})`,
           backgroundPosition: 'center',
         }}
       >
@@ -21,20 +25,20 @@ export function Hero() {
 
       {/* Conteúdo da sessão hero */}
       <div className="relative z-20 mx-auto max-w-4xl px-10 text-center">
-        <h1 className="mb-8 font-bold font-playfair text-4xl text-white drop-shadow-lg md:text-6xl lg:text-7xl">
+        <h1 className="mb-8 font-bold font-playfair text-4xl text-white drop-shadow-lg sm:text-5xl md:text-6xl lg:text-7xl">
           {title}
         </h1>
         {/* <p className="text-xl md:text-2xl text-white/90 mb-8 drop-shadow-md max-w-2xl mx-auto">
           Uma frase agradável aos olhos do cliente
         </p> */}
         <div className="flex flex-col justify-center gap-4 sm:flex-row">
-          {button.map((btn) => (
+          {buttons.map(({ info }) => (
             <button
-              key={btn.info}
+              key={info}
               type="button"
               className="mb-4 transform rounded-full bg-amber-900 px-8 py-3 font-medium text-lg text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-amber-900/80 focus:outline-none focus:ring-4 focus:ring-amber-bg-amber-900/50"
             >
-              {btn.info}
+              {info}
             </button>
           ))}
         </div>
---
+++ src/components/sections/hero/index.ts
@@ -0,0 +1,2 @@
+export * from './heroLayout';
+export * from './hero';
